3caef71a2c616a9e4d661e47163b43aa00ebd71a
aminbenarieb <amin.benarieb@gmail.com>
2016-06-04 14:09:33 +0300

    Concrete commands interfaces implemented.

---
+++ concretecommand.h
@@ -0,0 +1,65 @@
+#ifndef CONCRETECOMMAND_H
+#define CONCRETECOMMAND_H
+
+#include "basecommand.h"
+#include "action.h"
+
+class DrawScene : public BaseCommand
+{
+private:
+    Action *action;
+public:
+    DrawScene(Action*);
+    virtual void execute();
+};
+
+class ClearScene : public BaseCommand
+{
+private:
+    Action *action;
+public:
+    ClearScene(Action*);
+    virtual void execute();
+};
+
+class AddModel : public BaseCommand
+{
+private:
+    Action *action;
+    StreamInfo streamInfo;
+public:
+    AddModel(Action*, StreamInfo);
+    virtual void execute();
+};
+
+class AddCamera : public BaseCommand
+{
+private:
+    Action *action;
+    StreamInfo streamInfo;
+public:
+    AddCamera(Action*, StreamInfo);
+    virtual void execute();
+};
+
+class TransformModel : public BaseCommand
+{
+private:
+    Action *action;
+    TransformInfo transformInfo;
+public:
+    TransformModel(Action*, TransformInfo);
+    virtual void execute();
+};
+
+class TransformCamera : public BaseCommand
+{
+private:
+    Action *action;
+    TransformInfo transformInfo;
+public:
+    TransformCamera(Action*, TransformInfo);
+    virtual void execute();
+};
+
+#endif // CONCRETECOMMAND_H
