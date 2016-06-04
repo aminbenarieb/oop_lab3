73c710f61628ed651a8356134303aa3190999878
aminbenarieb <amin.benarieb@gmail.com>
2016-06-04 13:49:11 +0300

    Composite implemented.

---
+++ compositeobject.cpp
@@ -0,0 +1,60 @@
+#include "compositeobject.h"
+
+CompositeObject::CompositeObject() {}
+CompositeObject::~CompositeObject()
+{
+    this->removeAll();
+}
+
+
+size_t CompositeObject::getSize()
+{
+    return this->objects.size();
+}
+
+
+void CompositeObject::addObject(BaseObject *object)
+{
+    if (object)
+    {
+        this->objects.push_front(object);
+    }
+}
+void CompositeObject::removeAll()
+{
+    Iterator<BaseObject*> iterator(this->objects);
+
+    while(!iterator.isDone())
+    {
+        delete *iterator;
+        iterator.del();
+    }
+
+    if (this->getSize())
+    {
+        this->objects.clear();
+    }
+}
+void CompositeObject::transform(const BaseTransformer &transformer)
+{
+
+    Iterator<BaseObject*> iterator(this->objects);
+    while(!iterator.isDone())
+    {
+        (*iterator)->transform(transformer);
+    }
+}
+
+
+Iterator<BaseObject *> CompositeObject::begin()
+{
+    Iterator<BaseObject*> iterator(this->objects);
+    iterator.begin();
+    return iterator;
+}
+Iterator<BaseObject *> CompositeObject::end()
+{
+    Iterator<BaseObject*> iterator(this->objects);
+    iterator.end();
+    return iterator;
+}
